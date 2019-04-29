082e38b5ee5c2df994106779174e6aed38c4a1af
vincentmuya <vincentmuya13@gmail.com>
2019-04-29 17:04:01 +0300

    modified project settings.py,created cocky database,modified urls.py for app and project and created first view and imporeted lib in views

--- project/app/views.py
+++ project/app/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect, get_object_or_404
+from django.http import HttpResponse, Http404,HttpResponseRedirect
+from django.contrib.auth.decorators import login_required
+from django.http import JsonResponse
 
 # Create your views here.
+def index(request):
+    return render(request, 'index.html')
