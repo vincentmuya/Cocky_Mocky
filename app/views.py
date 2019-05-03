4f1427c14e51cc37291b54ee0d714c82e8626545
vincentmuya <vincentmuya13@gmail.com>
2019-05-03 21:53:44 +0300

    created about url and view function

--- app/views.py
+++ app/views.py
@@ -6,3 +6,6 @@ from django.http import JsonResponse
 # Create your views here.
 def index(request):
     return render(request, 'index.html')
+
+def about(request):
+    return render(request, 'about.html')
