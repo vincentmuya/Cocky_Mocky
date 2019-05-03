0d50b20ac51732a10486930814fc041b4ab5a7d4
vincentmuya <vincentmuya13@gmail.com>
2019-05-04 00:32:34 +0300

    added services url,view function and template

--- app/urls.py
+++ app/urls.py
@@ -6,4 +6,5 @@ from . import views
 urlpatterns=[
     url(r'^$',views.index,name ='index'),
     url(r'^about/', views.about, name='about'),
+    url(r'^services/', views.services, name='services')
     ]
--- app/views.py
+++ app/views.py
@@ -9,3 +9,6 @@ def index(request):
 
 def about(request):
     return render(request, 'about.html')
+
+def services(request):
+    return render(request, 'services.html')
